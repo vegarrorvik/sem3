a22ce37c1777f214f03966b4cf98253e07a442e3
Vegar <Vegar@mac>
2015-10-30 16:43:55 +0100

    added files

--- src/DepthFirstSearch.java
+++ src/DepthFirstSearch.java
@@ -1,5 +1,65 @@
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
- * Created by Vegar on 30.10.2015.
+ * Created by Vegar on 30.10.2015. - copied from Algs4 to be able to edit the code
  */
 public class DepthFirstSearch {
+    private boolean[] marked;    // marked[v] = is there an s-v path?
+    private int count;           // number of vertices connected to s
+
+    /**
+     * Computes the vertices in graph <tt>G</tt> that are
+     * connected to the source vertex <tt>s</tt>.
+     *
+     * @param G the graph
+     * @param s the source vertex
+     */
+    public DepthFirstSearch(Graph G, int s) {
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    // depth first search from v
+    private void dfs(Graph G, int v) {
+        count++;
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                System.out.println(v + " - " + w);
+                dfs(G, w);
+            }
+        }
+    }
+
+    /**
+     * Is there a path between the source vertex <tt>s</tt> and vertex <tt>v</tt>?
+     *
+     * @param v the vertex
+     * @return <tt>true</tt> if there is a path, <tt>false</tt> otherwise
+     */
+    public boolean marked(int v) {
+        return marked[v];
+    }
+
+    /**
+     * Returns the number of vertices connected to the source vertex <tt>s</tt>.
+     *
+     * @return the number of vertices connected to the source vertex <tt>s</tt>
+     */
+    public int count() {
+        return count;
+    }
+
+    /**
+     *
+     * @return marked for public use
+     */
+    public boolean[] getMarked(){
+        return marked;
+    }
 }
--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -1,6 +1,133 @@
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 /**
  * Created by Vegar on 30.10.2015.
  */
 public class MazeRunner {
+    static ArrayList maze = new ArrayList();
+    static File file = new File("compAss3/data/sm20x30.txt");
+    static In in = new In(file);
+    static Graph graph = new Graph(600);
+    static DepthFirstSearch dfs;
+    static ArrayList<Integer> exits = new ArrayList();
 
+
+    public static void main(String[]args){
+        createMaze();
+        makeEdges();
+        dfs();
+        findPossibleExits();
+        System.out.println(connected(598));
+    }
+
+    /**
+     * Creates an ArrayList maze, with all vertices
+     */
+    private static void createMaze(){
+        while(in.hasNextChar()) {
+            maze.add(in.readChar());
+        }
+
+        //Fikk problem med å gjere dette mens eg las inn
+        for(int i = 0; i<maze.size();i++){
+            if(maze.get(i).equals('\n'))
+                maze.remove(i);
+        }
+        //Because screw number 600
+        maze.remove(600);
+    }
+
+    /**
+     * Finds possible exits
+     */
+    private static void findPossibleExits(){
+        for(int i = 0; i<maze.size();i++){
+            if(i < 30 && maze.get(i).equals('1'))
+                exits.add(i);
+            if(i>0)
+                if(i%30==0 && maze.get(i-1).equals('1'))
+                    exits.add(i-1);
+            if(i%30==0 && maze.get(i).equals('1'))
+                exits.add(i);
+            if(i>569 && i<601 && maze.get(i).equals('1'))
+                exits.add(i);
+        }
+        System.out.println("\nPossible exits: ");
+        for(int i : exits){
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * Prints the maze in the correct way
+     */
+    private static void printMaze(){
+        for(int i = 0; i<maze.size(); i++){
+            if(i%30==0 && i!=0) {
+                System.out.print("\n" + maze.get(i));
+            }
+            else
+                System.out.print(maze.get(i));
+        }
+    }
+
+    /**
+     * Creating edges between connected points
+     */
+    private static void makeEdges(){
+        for(int i = 0;i<maze.size();i++){
+            if(i>0) {
+                if (maze.get(i).equals('1') && maze.get(i - 1).equals('1') && i > 0 && i%30 != 0)
+                    graph.addEdge(i, i - 1);
+            }
+            if(i>29) {
+                if (maze.get(i).equals('1') && maze.get(i - 30).equals('1') && i > 29)
+                    graph.addEdge(i, i - 30);
+            }
+        }
+    }
+
+    /**
+     * A depth first search, prints the number off connected vertices
+     */
+    private static void dfs(){
+        dfs = new DepthFirstSearch(graph,1);
+        System.out.println(dfs.count());
+    }
+
+    /**
+     * Doesn't work at the moment
+     */
+    private static void printMarked(){
+        final String ANSI_RESET = "\u0018[0m";
+        final String ANSI_RED = "\u0018[31m";
+        for(int i = 0; i<maze.size(); i++){
+            if(i%30==0 && i!=0) {
+                System.out.println();
+                if(dfs.getMarked()[i])
+                    System.out.print(ANSI_RED + maze.get(i) + ANSI_RESET);
+                else
+                    System.out.print(maze.get(i));
+            }
+            else
+                if(dfs.getMarked()[i])
+                    System.out.print(ANSI_RED + maze.get(i) + ANSI_RESET);
+                else
+                    System.out.print(maze.get(i));
+        }
+    }
+
+    /**
+     *
+     * @param v check if this is connected
+     * @return true or false
+     */
+    private static boolean connected(int v){
+        return dfs.marked(v);
+    }
 }
